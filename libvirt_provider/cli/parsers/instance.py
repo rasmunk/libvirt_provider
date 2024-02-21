6698b913c382651f80ac2a1003e455a1de6d5425
Rasmus Munk <munk1@live.dk>
2024-02-21 14:32:05 +0100

    Add help messages

--- libvirt_provider/cli/parsers/instance.py
+++ libvirt_provider/cli/parsers/instance.py
@@ -8,13 +8,14 @@ def valid_create_group(parser):
 
 def create_group(parser):
     instance_group = parser.add_argument_group(title="Instance create arguments")
-    instance_group.add_argument("name", action=PositionalArgumentsAction)
-    instance_group.add_argument("disk_image_path", action=PositionalArgumentsAction)
+    instance_group.add_argument("name", action=PositionalArgumentsAction, help="The name of the instance")
+    instance_group.add_argument("disk_image_path", action=PositionalArgumentsAction, help="The path to the disk image")
     instance_group.add_argument(
-        "-ms", "--memory-size", dest="{}_memory_size".format(INSTANCE), default="1024"
+        "-ms", "--memory-size", dest="{}_memory_size".format(INSTANCE), default="1024",
+        help="The memory size of the instance"
     )
 
 
 def remove_group(parser):
     instance_group = parser.add_argument_group(title="Instance remove arguments")
-    instance_group.add_argument("id")
+    instance_group.add_argument("id", help="The id of the instance to be removed")
