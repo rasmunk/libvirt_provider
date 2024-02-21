379c50ad7ab921fe9aa8d47a0c9ff7e7ea8c67fe
Rasmus Munk <munk1@live.dk>
2024-02-21 14:32:34 +0100

    Reformat

--- libvirt_provider/cli/parsers/instance.py
+++ libvirt_provider/cli/parsers/instance.py
@@ -8,11 +8,20 @@ def valid_create_group(parser):
 
 def create_group(parser):
     instance_group = parser.add_argument_group(title="Instance create arguments")
-    instance_group.add_argument("name", action=PositionalArgumentsAction, help="The name of the instance")
-    instance_group.add_argument("disk_image_path", action=PositionalArgumentsAction, help="The path to the disk image")
     instance_group.add_argument(
-        "-ms", "--memory-size", dest="{}_memory_size".format(INSTANCE), default="1024",
-        help="The memory size of the instance"
+        "name", action=PositionalArgumentsAction, help="The name of the instance"
+    )
+    instance_group.add_argument(
+        "disk_image_path",
+        action=PositionalArgumentsAction,
+        help="The path to the disk image",
+    )
+    instance_group.add_argument(
+        "-ms",
+        "--memory-size",
+        dest="{}_memory_size".format(INSTANCE),
+        default="1024",
+        help="The memory size of the instance",
     )
 
 
