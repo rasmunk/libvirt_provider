b4d26ebc7e684eec6575ae83a01fb6d44f6c02b5
Rasmus Munk <munk1@live.dk>
2024-02-27 10:09:36 +0100

    Add CLI instance create arguments

--- libvirt_provider/cli/parsers/instance.py
+++ libvirt_provider/cli/parsers/instance.py
@@ -17,11 +17,74 @@ def create_group(parser):
         help="The path to the disk image",
     )
     instance_group.add_argument(
+        "-dt",
+        "--domain-type",
+        dest="{}_domain_type".format(INSTANCE),
+        default="kvm",
+        help="The domain type",
+    )
+    instance_group.add_argument(
+        "-dt",
+        "--disk-type",
+        dest="{}_disk_type".format(INSTANCE),
+        default="file",
+        help="The disk type",
+    )
+    instance_group.add_argument(
+        "-ddt",
+        "--disk-driver-type",
+        dest="{}_disk_driver_type".format(INSTANCE),
+        default="qcow2",
+        help="The disk driver type",
+    )
+    instance_group.add_argument(
+        "-dtd",
+        "--disk-target-dev",
+        dest="{}_disk_target_dev".format(INSTANCE),
+        default="hda",
+        help="The disk target device",
+    )
+    instance_group.add_argument(
+        "-dtb",
+        "--disk-target-bus",
+        dest="{}_disk_target_bus".format(INSTANCE),
+        default="ide",
+        help="The disk target bus",
+    )
+    instance_group.add_argument(
         "-ms",
         "--memory-size",
         dest="{}_memory_size".format(INSTANCE),
         default="1024",
         help="The memory size of the instance",
+    )
+    instance_group.add_argument(
+        "-vs",
+        "--num-vcpus",
+        dest="{}_num_vcpus".format(INSTANCE),
+        default="1",
+        help="The number of virtual CPUs",
+    )
+    instance_group.add_argument(
+        "-cpu-arch",
+        "--cpu-architecture",
+        dest="{}_cpu_architecture".format(INSTANCE),
+        default="x86_64",
+        help="The CPU architecture",
+    )
+    instance_group.add_argument(
+        "-mach",
+        "--machine",
+        dest="{}_machine".format(INSTANCE),
+        default="pc",
+        help="The machine type",
+    )
+    instance_group.add_argument(
+        "-ct",
+        "--console-type",
+        dest="{}_console_type".format(INSTANCE),
+        default="pty",
+        help="The console type",
     )
 
 
