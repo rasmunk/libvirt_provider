f710df5f28c412e238936c10eb888a6f0f46d057
Rasmus Munk <munk1@live.dk>
2024-06-10 11:38:10 +0200

    Adjust the test_libvirt_pool to use the new shshared TestContext

--- tests/smoke/test_libvirt_pool.py
+++ tests/smoke/test_libvirt_pool.py
@@ -1,85 +1,32 @@
 import unittest
 import pytest
 import os
-import wget
-from libvirt_provider.utils.io import remove as remove_file
-from libvirt_provider.utils.io import (
-    copy,
-    exists,
-    join,
-    makedirs,
-    load_json,
-    chown,
-    chmod,
-    get_uid,
-    get_gid,
-    access,
-)
-from libvirt_provider.utils.user import (
-    lookup_uid,
-    lookup_gid,
-    find_user_with_username,
-    find_group_with_groupname,
-)
+import random
+from libvirt_provider.utils.io import copy, exists, join, load_json
 from libvirt_provider.defaults import LIBVIRT
 from libvirt_provider.client import new_client
 from libvirt_provider.pool import Pool
 from libvirt_provider.models import Node
 from libvirt_provider.instance.create import create
 from libvirt_provider.instance.remove import remove
+from .context import TestContext
 
 
 @pytest.mark.smoke
 class TestLibvirtPool(unittest.IsolatedAsyncioTestCase):
+    context = TestContext()
+
     async def asyncSetUp(self):
-        user_base = "qemu"
-        self.user = find_user_with_username(user_base)
-        self.assertIsNot(self.user, False)
-        self.group = find_group_with_groupname(user_base)
-        self.assertIsNot(self.group, False)
+        await self.context.setUp()
 
-        self.architecture = "x86_64"
-        self.name = f"libvirt-pool-{self.architecture}"
-        # Note, a properly SELinux labelled directory is required when SELinux is enabled
-        # self.images_dir = join(
-        #    os.sep, "var", "lib", "libvirt", "images", self.architecture
-        # )
-        self.images_dir = join("tests", "images", self.architecture)
-        if not exists(self.images_dir):
-            self.assertTrue(makedirs(self.images_dir))
+        self.seed = str(random.random())[2:10]
+        self.name = f"libvirt-pool-{self.context.architecture}-{self.seed}"
 
-        self.image = join(self.images_dir, f"{self.name}-Rocky-9.qcow2")
-        if not exists(self.image):
-            # Download the image
-            url = f"https://download.rockylinux.org/pub/rocky/9/images/{self.architecture}/Rocky-9-GenericCloud-Base.latest.{self.architecture}.qcow2"
-            try:
-                print(f"Downloading image: {url} for testing")
-                wget.download(url, self.image)
-            except Exception as err:
-                print(f"Failed to download image: {url} - {err}")
-                self.assertFalse(True)
-        self.assertTrue(exists(self.image))
-
-        qemu_uid, qemu_gid = lookup_uid(self.user), lookup_gid(self.group)
-        self.assertIsNot(qemu_uid, False)
-        self.assertIsNot(qemu_gid, False)
-
+        self.test_image = os.path.realpath(f"{self.context.image}-{self.seed}")
+        self.assertTrue(copy(self.context.image, self.test_image))
+        self.assertTrue(exists(self.test_image))
         open_uri = "qemu:///session"
         self.client = new_client(LIBVIRT, open_uri=open_uri)
-        for i in range(2):
-            test_image = join(self.images_dir, f"{self.name}-Rocky-9-{i}.qcow2")
-            if not exists(test_image):
-                self.assertTrue(copy(self.image, test_image))
-            self.assertTrue(exists(test_image))
-            # Ensure correct ownership on image file
-            existing_uid, existing_gid = get_uid(test_image), get_gid(test_image)
-            self.assertIsNot(existing_uid, False)
-            self.assertIsNot(existing_gid, False)
-            if existing_uid != qemu_uid or existing_gid != qemu_gid:
-                self.assertTrue(chown(test_image, qemu_uid, qemu_gid))
-            if not access(test_image, os.R_OK | os.X_OK):
-                self.assertTrue(chmod(test_image, 0o755))
-            self.assertTrue(access(test_image, os.R_OK | os.X_OK))
 
     async def asyncTearDown(self):
         # Ensure that any pool is destroyed
@@ -93,57 +40,59 @@ class TestLibvirtPool(unittest.IsolatedAsyncioTestCase):
         self.assertTrue(await pool.remove_persistence())
         self.client.close()
 
-        for i in range(2):
-            test_image = join(self.images_dir, f"{self.name}-Rocky-9-{i}.qcow2")
-            self.assertTrue(remove_file(test_image))
-            self.assertFalse(exists(test_image))
+    @classmethod
+    def tearDownClass(cls):
+        cls.context.tearDown()
 
     async def test_libvirt_pool(self):
-        for i in range(2):
-            test_image = os.path.abspath(join(self.images_dir, f"Rocky-9-{i}.qcow2"))
-            self.assertTrue(copy(self.image, test_image))
-            self.assertTrue(exists(test_image))
-
-            # Load architecture node_options
-            node_options_path = join(
-                "tests", "res", "node_options", f"{self.architecture}.json"
+        # Load architecture node_options
+        node_options_path = os.path.realpath(
+            join(
+                "tests",
+                "smoke",
+                "res",
+                "node_options",
+                f"{self.context.architecture}.json",
             )
-            loaded_node_options = load_json(node_options_path)
-            self.assertIsInstance(loaded_node_options, dict)
+        )
+        self.assertTrue(exists(node_options_path))
+        loaded_node_options = load_json(node_options_path)
+        self.assertIsInstance(loaded_node_options, dict)
 
-            pool = Pool(self.name)
-            self.assertIsNotNone(pool)
-            self.assertEqual(pool.name, self.name)
-            self.assertEqual(len(await pool.items()), 0)
+        pool = Pool(self.name)
+        self.assertIsNotNone(pool)
+        self.assertEqual(pool.name, self.name)
+        self.assertEqual(len(await pool.items()), 0)
 
-            node_options = {
-                "name": f"libvirt-test-{i}",
-                "disk_image_path": test_image,
-                "memory_size": "4096",
-                **loaded_node_options,
-            }
+        node_name = f"libvirt-test-{self.seed}-node"
+        node_options = {
+            "name": node_name,
+            "disk_image_path": self.test_image,
+            "memory_size": "1024MiB",
+            **loaded_node_options,
+        }
 
-            create_success, create_response = await create(self.client, **node_options)
-            self.assertTrue(create_success)
-            self.assertIn("instance", create_response)
-            self.assertIsNotNone(create_response["instance"])
-            node = create_response["instance"]
+        create_success, create_response = await create(self.client, **node_options)
+        self.assertTrue(create_success)
+        self.assertIn("instance", create_response)
+        self.assertIsNotNone(create_response["instance"])
+        node = create_response["instance"]
 
-            self.assertIsInstance(node, Node)
-            self.assertEqual(node.name, node_options["name"])
-            self.assertTrue(await pool.add(node))
-            self.assertEqual(len(await pool.items()), 1)
+        self.assertIsInstance(node, Node)
+        self.assertEqual(node.name, node_options["name"])
+        self.assertTrue(await pool.add(node))
+        self.assertEqual(len(await pool.items()), 1)
 
-            pool_node = await pool.get(node.id)
-            self.assertIsInstance(pool_node, Node)
-            self.assertEqual(pool_node.name, node_options["name"])
+        pool_node = await pool.get(node.id)
+        self.assertIsInstance(pool_node, Node)
+        self.assertEqual(pool_node.name, node_options["name"])
 
-            self.assertTrue(await pool.remove(node.id))
-            self.assertFalse(await pool.get(node.id))
-            self.assertEqual(len(await pool.items()), 0)
+        self.assertTrue(await pool.remove(node.id))
+        self.assertFalse(await pool.get(node.id))
+        self.assertEqual(len(await pool.items()), 0)
 
-            removed, remove_response = await remove(self.client, node.id)
-            self.assertTrue(removed)
+        removed, remove_response = await remove(self.client, node.id)
+        self.assertTrue(removed)
 
 
 if __name__ == "__main__":
